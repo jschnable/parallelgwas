0c31cd9f20bd12b819a79d5249a7c25935111b08
jdavis-132 <jdavis132@huskers.unl.edu>
2025-04-18 16:10:34 -0500

    Fix bugs in manhattan plot function

--- manhattanPlot.R
+++ manhattanPlot.R
@@ -5,7 +5,6 @@ library(scales)
 # multitrait: logical indicating if the plot should represent GWAS results for multiple phenotypes
 # trait: column indicating which trait the marker significance value is for if multitrait is TRUE
 # resampling: logical indicating if the data is from a resampling-based GWAS or not
-# chromLengths: vector of number of basepairs in each chromosome; if NULL, this is estimated based on markers in data
 # chr: name of column with chromosome numbers 
 # bp: name of column with basepair number for each marker within its chromosome
 # threshold: position of significance threshold to plot; default is 0.1, assuming the default of resampling = TRUE
@@ -13,13 +12,15 @@ library(scales)
 # colors: vector of hex codes or strings of color names in the R color list to use; if provided, must be the length of either the number of chromosomes (if multitrait=FALSE) or the number of traits (if multitrait = TRUE)
 # theme: object containing theme info to use instead of theme elements hard-coded here
 # species: name of species; currently supported: 'maize', 'sorghum'
-plotManhattan <- function(data, sig, multitrait=FALSE, trait=NULL, resampling=TRUE, chromLengths=NULL, chr=CHROM, bp=POS, threshold=0.1, main=NULL, colors=NULL, theme=NULL, species)
+plotManhattan <- function(data, sig, multitrait=FALSE, trait=NULL, resampling=TRUE, chr=CHROM, bp=POS, threshold=0.1, main=NULL, colors=NULL, theme=NULL, species)
 {
   ylab <- 'RMIP'
   theme_use <- theme
-
-  if(species=='maize' & is.null(chromLengths)){chromLength <-  tibble(max_bp = c(308452471, 243675191, 238017767, 250330460, 226353449, 181357234, 185808916, 182411202, 163004744, 152435371), {{ chr }} := 1:10)}
-  if(species=='sorghum' & is.null(chromLengths)){chromLength <- tibble(max_bp = c(85112863, 79114963, 80873341, 71215609, 77058072, 62713908, 68911884, 65779274, 63277606, 62870657), {{ chr }} := 1:10)}
+  chromLength <- tibble()
+  if(species=='maize'){chromLength <-  tibble(max_bp = c(308452471, 243675191, 238017767, 250330460, 226353449, 181357234, 185808916, 182411202, 163004744, 152435371), {{ chr }} := 1:10)}
+  if(species=='sorghum'){chromLength <- tibble(max_bp = c(85112863, 79114963, 80873341, 71215609, 77058072, 62713908, 68911884, 65779274, 63277606, 62870657), {{ chr }} := 1:10)}
+  n_chromosomes <- length(chromLength$max_bp)
+  last_chr_len <- chromLength$max_bp[n_chromosomes]
   
   if(is.null(theme))
   {
@@ -36,24 +37,15 @@ plotManhattan <- function(data, sig, multitrait=FALSE, trait=NULL, resampling=TR
   }
   
   data_cum <- data %>%
-    group_by( {{ chr }}) %>%
-    summarise(max_bp = max({{ bp }}, na.rm = TRUE)) %>%
-    mutate(bp_add = lag(cumsum(max_bp), default = 0)) %>%
-    select(c({{ chr }}, bp_add))
-  
-  if(!is.null(chromLengths))
-  {
-    data_cum <- data %>%
       group_by({{ chr }}) %>%
       summarise(n_snps = n()) %>%
-      full_join(chromLengths, join_by('{chr}')) %>%
+      full_join(chromLength, join_by( {{ chr }})) %>%
       arrange({{ chr }}) %>% 
       mutate(bp_add = lag(cumsum(max_bp), default = 0)) %>%
       select(c({{ chr }}, bp_add))
-  }
   
   df <- data %>% 
-    inner_join(data_cum, join_by({{ chr }})) %>%
+    full_join(data_cum, join_by({{ chr }})) %>%
     rowwise() %>%
     mutate(loc = {{ bp }} + bp_add)
 
@@ -64,11 +56,11 @@ plotManhattan <- function(data, sig, multitrait=FALSE, trait=NULL, resampling=TR
        rowwise() %>%
        mutate({{ sig }} := -1*log10({{ sig }}))
    }
-  
-  x_axis_set <- df %>% 
-    group_by({{ chr }}) %>% 
-    summarise(center = mean(loc, na.rm = TRUE))
-  
+  xlimit <- last_chr_len + max(data_cum$bp_add)
+  x_axis_set <- data_cum %>% 
+    arrange({{ chr }}) %>% 
+    mutate(center = (bp_add + lead(bp_add, default = xlimit))/2)
+
   if(multitrait)
   {
     if(is.null(colors))
@@ -81,8 +73,9 @@ plotManhattan <- function(data, sig, multitrait=FALSE, trait=NULL, resampling=TR
     manhattan <- ggplot(df, aes(loc, {{ sig }}, color = as.factor(.data[[deparse(substitute(trait))]]))) + 
       geom_point() + 
       geom_hline(yintercept = threshold, linetype = 2, color = 'black') +
-      scale_x_continuous(label = x_axis_set[[deparse(substitute(chr))]], 
-                         breaks = x_axis_set$center) +
+      scale_x_continuous(labels = chromLength[[deparse(substitute(chr))]], 
+                         breaks = x_axis_set$center, 
+                         limits = c(0, xlimit)) +
       scale_color_manual(values = colors) + 
       labs(title = main, x = 'Chromosome', y = ylab, color = NULL) + 
       theme_use + 
@@ -99,8 +92,9 @@ plotManhattan <- function(data, sig, multitrait=FALSE, trait=NULL, resampling=TR
     manhattan <- ggplot(df, aes(loc, {{ sig }}, color = as.factor({{ chr }}))) + 
       geom_point() + 
       geom_hline(yintercept = threshold, linetype = 2, color = 'black') +
-      scale_x_continuous(label = x_axis_set[[deparse(substitute(chr))]], 
-                         breaks = x_axis_set$center) +
+      scale_x_continuous(labels = chromLength[[deparse(substitute(chr))]], 
+                         breaks = x_axis_set$center, 
+                       limits = c(0, xlimit)) +
       scale_color_manual(values = colors) + 
       labs(title = main, x = 'Chromosome', y = ylab, color = NULL) + 
       theme_use
